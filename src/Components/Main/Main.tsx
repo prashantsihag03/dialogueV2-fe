9ce95aced853d0a305a7902ddefc8d02355aa0d2
Prashant Sihag <prashantsihag0309@gmail.com>
2023-03-02 23:19:14 +1030

    created two skeleton sections to hold chatbox and sidebar, added styles for them

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,6 +1,21 @@
 import { Box } from '@mui/material'
-import { containerStyles } from './styles'
+import { ChatBox } from '../ChatBox/ChatBox'
+import {
+  chatBoxSectionStyles,
+  containerStyles,
+  sectionStyles,
+  sideBarSectionStyles,
+} from './styles'
 
 export const Main: React.FC = () => {
-  return <Box sx={containerStyles}></Box>
+  return (
+    <Box sx={containerStyles}>
+      <Box sx={{ ...sectionStyles, ...chatBoxSectionStyles }}>
+        <ChatBox />
+      </Box>
+      <Box sx={{ ...sectionStyles, ...sideBarSectionStyles }}>
+        <ChatBox />
+      </Box>
+    </Box>
+  )
 }
--- src/Components/Main/styles.ts
+++ src/Components/Main/styles.ts
@@ -4,7 +4,27 @@ import { Theme } from '@mui/system'
 export const containerStyles: SxProps<Theme> = {
   width: '100%',
   padding: '0% 1%',
-  height: '92%',
+  height: '90%',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
   backgroundColor: 'transparent',
-  border: '1px solid white',
+}
+
+export const sectionStyles: SxProps<Theme> = {
+  height: '100%',
+  backgroundColor: 'background.paper',
+  borderRadius: '1vw',
+}
+
+export const sideBarSectionStyles: SxProps<Theme> = {
+  borderRadius: '1vw',
+  flex: 1,
+}
+
+export const chatBoxSectionStyles: SxProps<Theme> = {
+  flex: 3,
+  marginRight: '10px',
+  borderRadius: '1vw',
 }
