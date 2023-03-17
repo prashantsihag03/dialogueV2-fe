e564a2a9b98f2e916f25c5766ac59776bed21e11
Prashant Sihag <prashantsihag0309@gmail.com>
2023-03-18 02:33:09 +1030

    added emoji picker and ability to input emojis in message input box

--- src/Components/ChatBox/MessageInputBox/MessageInputBox.tsx
+++ src/Components/ChatBox/MessageInputBox/MessageInputBox.tsx
@@ -1,16 +1,48 @@
-import { Box, TextField } from '@mui/material'
+import { Box, ClickAwayListener, TextField } from '@mui/material'
 import InsertEmoticonOutlinedIcon from '@mui/icons-material/InsertEmoticonOutlined'
 import SendRoundedIcon from '@mui/icons-material/SendRounded'
 import AttachFileRoundedIcon from '@mui/icons-material/AttachFileRounded'
 import MicNoneOutlinedIcon from '@mui/icons-material/MicNoneOutlined'
-
 import { iconStyles, messageBox, messageOptions } from './styles'
+import EmojiPicker, { Theme, EmojiClickData } from 'emoji-picker-react'
+import { useEffect, useState } from 'react'
 
 export const MessageInputBox: React.FC = () => {
+  const [message, setMessage] = useState<string>('')
+  const [showEmojis, setShowEmojis] = useState<boolean>(false)
+  const [emoticonEle, setEmoticonEle] = useState<SVGSVGElement | null>(null)
+
+  const [emojiPickerLeftpos, setEmojiPickerLeftpos] = useState<number | null>(
+    null
+  )
+  const [emojiPickerBottompos, setEmojiPickerBottompos] = useState<
+    number | null
+  >(null)
+
+  const addEmojiToInput = (emojiObject: EmojiClickData) => {
+    setMessage(
+      message + String.fromCodePoint(Number(`0x${emojiObject.unified}`))
+    )
+  }
+
+  useEffect(() => {
+    if (emoticonEle) {
+      const rect = emoticonEle.getBoundingClientRect()
+      setEmojiPickerLeftpos(rect.x)
+      setEmojiPickerBottompos(window.innerHeight - rect.y + 20)
+    }
+  }, [emoticonEle])
+
   return (
     <Box sx={messageOptions}>
       <Box sx={messageBox}>
-        <InsertEmoticonOutlinedIcon titleAccess="Emoticons" />
+        <InsertEmoticonOutlinedIcon
+          titleAccess="Emoticons"
+          ref={(node) => setEmoticonEle(node)}
+          onClick={() => {
+            setShowEmojis(!showEmojis)
+          }}
+        />
         <TextField
           id="standard-textarea"
           variant="standard"
@@ -18,6 +50,10 @@ export const MessageInputBox: React.FC = () => {
           multiline
           fullWidth
           maxRows={6}
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value)
+          }}
           InputProps={{ disableUnderline: true }}
           sx={{ marginLeft: '0.5rem' }}
         />
@@ -35,6 +71,25 @@ export const MessageInputBox: React.FC = () => {
         sx={iconStyles}
         titleAccess="send message"
       />
+      {emojiPickerLeftpos != null &&
+      emojiPickerBottompos != null &&
+      showEmojis ? (
+        <ClickAwayListener
+          onClickAway={() => {
+            setShowEmojis(false)
+          }}
+        >
+          <Box
+            sx={{
+              position: 'absolute',
+              left: `${emojiPickerLeftpos}px`,
+              bottom: `${emojiPickerBottompos}px`,
+            }}
+          >
+            <EmojiPicker theme={Theme.AUTO} onEmojiClick={addEmojiToInput} />
+          </Box>
+        </ClickAwayListener>
+      ) : null}
     </Box>
   )
 }
