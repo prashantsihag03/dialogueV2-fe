a309a6b2fdd105eaf7b4c09c7bf09f3ad84c40ed
Prashant Sihag <prashantsihag0309@gmail.com>
2023-03-25 13:55:59 +1030

    added basic connection setup for socket, created skeleton script to copy build files to backend for dev env

---
+++ scripts/copyBuildFilesToBackend.sh
@@ -0,0 +1,16 @@
+# add bash declaration here
+
+# use environment variable to assign Backend build path to this variable
+BE_BUILD_PATH=""
+
+BE_BUILD_JAVASCRIPT_PATH="${BE_BUILD_PATH}/javascript"
+
+BE_BUILD_IMAGES_PATH="${BE_BUILD_PATH}/images"
+
+# move bundle.js to BE_BUILD_JAVASCRIPT_PATH
+# move bundle.js.LICENSE.txt to BE_BUILD_JAVASCRIPT_PATH
+# move bundle.js.map to BE_BUILD_JAVASCRIPT_PATH
+
+# move all files from images to BE_BUILD_IMAGES_PATH
+
+# move index.html to BE_BUILD_PATH
