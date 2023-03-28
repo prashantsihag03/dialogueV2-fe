e9aa4c1ded46e2eca132d4cb4bd31828ba3e76f7
Prashant Sihag <prashantsihag0309@gmail.com>
2023-03-29 00:04:13 +1030

    updated script to copy build files to be

--- src/socket.ts
+++ src/socket.ts
@@ -3,6 +3,7 @@ import { io, Socket } from 'socket.io-client'
 let socket: Socket
 
 if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+  console.log('Dev Env detected. Connecting socket to localhost:3000')
   socket = io('http://localhost:3000/')
 } else {
   socket = io()
