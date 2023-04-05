4bbcafe707df7a4a36b23ba5d6dae2d5a6827a79
Prashant Sihag <prashantsihag0309@gmail.com>
2023-04-06 02:43:05 +0930

    setup API slice and added its reducer and middlewares to store, added placeholder useHook for a placeholder query in chats comp

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,18 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from './api/slice'
 import { chatsReducer } from './chats/slice'
 import { connectionReducer } from './connection/slice'
 
 const rootReducer = combineReducers({
   connection: connectionReducer,
   chats: chatsReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
 })
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
